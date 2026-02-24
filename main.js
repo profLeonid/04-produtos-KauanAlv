a3ad688e46781064072b7fef1fe2dd0d7d8cf97d
KAUAN ALVES PEREIRA <25203696@dsn127m1246731.cfp127aluno.local>
2026-02-24 15:20:01 -0300

    [wip] adicionar validação e alerta

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 'use strict'
 
-function adicionarProduto(){
+function adicionarProduto() {
     const produto = document.getElementById('produto')
     const lista = document.getElementById('lista')
-    
     const span = document.createElement('span')
-    span.textContent = produto.value
-    span.className = 'bg-blue-200 px-8 py-2 border rounded text-center'
 
-    //Vai adicionar o span na lista
-    lista.appendChild(span)
+    if (produto.value == "" || !isNaN(produto.value)) {
+        alert('ERRO!!! PRODUTO INSERIDO DE FORMA ERRADA!')
+        produto.value = ''
+    } else {
+        span.textContent = produto.value
+        span.className = 'bg-blue-200 px-8 py-2 border rounded text-center'
+
+        //Vai adicionar o span na lista
+        lista.appendChild(span)
+        produto.value = ''
+    }
 }
